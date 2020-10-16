49682bf723a1f02d0fe8feadff4f45fcc63f091e
shuaipengge <shuaipengge@gmail.com>
2020-10-16 23:11:23 +0800

    支持国际化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import i18n from './lang'
 // 导入字体图标
 import './assets/styles/icon.css'
 // 引入全局样式
@@ -14,5 +15,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  i18n,
   render: h => h(App)
 }).$mount('#app')
